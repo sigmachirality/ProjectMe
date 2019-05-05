e806c24217c5d01d46351850457b99524c3d2ed8
Daniel Tao <danieltaox@gmail.com>
2019-05-05 00:23:31 -0400

    Construct new minimalist home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,12 @@
 import React, { Component } from "react";
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import logo from '../assets/logo.png'
-import { Navbar, Container, Content } from 'react-bulma-components';
-import Footer from '../components/Footer'
+import '../assets/search.css'
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        <Navbar
-          className="is-dark"
-          style={{ height: "100px" }}
-        >
-          <Navbar.Brand>
-            <Navbar.Item>
-              <img
-                src={logo}
-                alt="ProjectMe"
-                style={{
-                  width: "171px",
-                  maxHeight: "100px"
-                }}
-              />
-            </Navbar.Item>
-          </Navbar.Brand>
-        </Navbar>
-        <div style={{ minHeight: "100vh" }}></div>
-        <Footer />
-      </div>
+      <div style={{ height: "100vh" }}></div>
     );
   }
 }
