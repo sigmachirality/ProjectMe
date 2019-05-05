f019ef1972656818303f1756f276683a6c192bff
Daniel Tao <danieltaox@gmail.com>
2019-05-05 01:41:55 -0400

    Center logo and inputs, fix background color

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Select from 'react-select'
+import { Button, Container, Columns, Image } from 'react-bulma-components';
+import Footer from '../components/Footer'
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import '../assets/search.css'
 import logo from '../assets/logo.png'
@@ -9,19 +11,37 @@ class App extends Component {
   render() {
     return (
       <div>
-        <img
-          src={logo}
-          className="logo"
-        />
-        <br />
-        <Select
-          isClearable
-          isSearchable
-        />
-        <Select
-          isClearable
-          isSearchable
-        />
+        <Container is-desktop>
+          <Image
+            src={logo}
+            className="logo"
+          />
+          <Columns>
+            <Columns.Column size={2} />
+            <Columns.Column size={3}>
+              <Select
+                placeholder="From..."
+                isClearable
+                isSearchable
+              />
+            </Columns.Column>
+            <Columns.Column size={3}>
+              <Select
+                placeholder="To..."
+                isClearable
+                isSearchable
+              />
+            </Columns.Column>
+            <Columns.Column size={2}>
+              <Button
+                fullwidth
+              >
+                Project Me!
+              </Button>
+            </Columns.Column>
+            <Columns.Column size={2} />
+          </Columns>
+        </Container>
       </div>
     );
   }
