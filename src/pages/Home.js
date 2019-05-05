d47d66743e2ff76cce9b477ea75a1bf30c6cff4d
Daniel Tao <danieltaox@gmail.com>
2019-05-05 01:03:06 -0400

    Add two searchable select elements

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
+import Select from 'react-select'
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import '../assets/search.css'
+import logo from '../assets/logo.png'
 
 class App extends Component {
 
   render() {
     return (
-      <div style={{ height: "100vh" }}></div>
+      <div>
+        <img
+          src={logo}
+          className="logo"
+        />
+        <br />
+        <inline-block>
+          <Select
+            style={{ width: "50vw" }}
+            isClearable
+            isSearchable
+          />
+          <Select
+            style={{ width: "50vw" }}
+            isClearable
+            isSearchable
+          />
+        </inline-block>
+      </div>
     );
   }
 }
